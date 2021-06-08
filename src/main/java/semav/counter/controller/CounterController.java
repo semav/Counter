9cd4048f2170e9454ba94204bd590cb61c3469c3
Alexander Semenov <1semav@gmail.com>
2021-06-08 21:49:13 +0300

    Контроллер для работы со счетчиками

--- src/main/java/semav/counter/controller/CounterController.java
+++ src/main/java/semav/counter/controller/CounterController.java
@@ -1,8 +1,44 @@
 package semav.counter.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import semav.counter.dto.CounterDto;
 
-@RestController
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Rest-контроллер для работы со счетчиками
+ */
+@RestController("counter")
 public class CounterController {
+    @PostMapping
+    public CounterDto create(CounterDto counter) {
+        return null;
+    }
+
+    @PostMapping("/{name}/increment")
+    public CounterDto increment(@PathVariable String name) {
+        return null;
+    }
+
+    @GetMapping("/{name}")
+    public CounterDto get(@PathVariable String name) {
+        return null;
+    }
+
+    @GetMapping
+    public List<CounterDto> getList() {
+        return Collections.emptyList();
+    }
+
+    @GetMapping("/sum")
+    public int getSum() {
+        return 0;
+    }
+
+    @DeleteMapping("/{name}")
+    public CounterDto delete(@PathVariable String name) {
+        return null;
+    }
 
 }
